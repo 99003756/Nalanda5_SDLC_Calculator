f52fdcf8ac5c52a7eb1a21df7ec21850dd841d41
99003763 <78864900+99003763@users.noreply.github.com>
2021-02-12 23:11:34 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main()
+{
+    int select_option;
+    double RadianValue;
+    double Degrees_Value;
+    int ask_User;
+    do
+    {
+        printf("Enter Degrees Value\n");
+        scanf("%lf",&Degrees_Value);
+        printf("Please only one option for Trigonometry Operation\n");
+        printf(" 1. Sin\n 2. cos\n 3. Tan\n 4. Sec\n 5. Cosec \n 6. Cot\n");
+        scanf("%d",&select_option);
+        RadianValue=0.017*Degrees_Value;
+        switch(select_option)
+        {
+
+            case 1: Sin(RadianValue);
+            break;
+            case 2: Cos(RadianValue);
+            break;
+            case 3: Tan(RadianValue);
+            break;
+            case 4: Sec(RadianValue);
+            break;
+            case 5: Cosec(RadianValue);
+            break;
+            case 6: Cot(RadianValue);
+            break;
+
+            default: printf("Inavlid Input!!!! Please choose Correct option\n");
+            break;
+        }
+
+        printf("DO You want calculate again? \n1. Yes 2. No\n");
+        scanf("%d",&ask_User);
+    }
+    while(ask_User==1);
+}
+    void Sin(double RadianValue ){
+        printf("\n %lf\n",sin(RadianValue));
+    }
+    void Cos(double RadianValue ){
+        printf("\n %lf\n",cos(RadianValue));
+    }
+    void Tan(double RadianValue ){
+        printf("\n %lf\n",tan(RadianValue));
+    }
+    void Cosec(double RadianValue ){
+        printf("\n %lf\n",(1.0/sin(RadianValue)));
+    }
+    void Sec(double RadianValue ){
+        printf("\n %lf\n",(1.0/cos(RadianValue)));
+    }
+    void Cot(double RadianValue ){
+        printf("\n %lf\n",(1.0/tan(RadianValue)));
+    }
