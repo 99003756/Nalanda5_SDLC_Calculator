9eb4b2b760095ae2c030ba76163a0144a64bc607
99003763 <78864900+99003763@users.noreply.github.com>
2021-02-13 15:04:50 +0530

    Trigno.c
    Commited by Rajat

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include<math.h>
+
+void Sin_function(double Degrees_Value);
+void Cos_function(double Degrees_Value);
+void Tan_function(double Degrees_Value);
+void Cosec_function(double Degrees_Value);
+void Sec_function(double Degrees_Value);
+void Cot_function(double Degrees_Value);
 
 int main()
 {
@@ -12,26 +18,25 @@ int main()
     {
         printf("Enter Degrees Value\n");
         scanf("%lf",&Degrees_Value);
-        printf("Please only one option for Trigonometry Operation\n");
+        printf("Please Choose  only one option for Trigonometry Operation\n");
         printf(" 1. Sin\n 2. cos\n 3. Tan\n 4. Sec\n 5. Cosec \n 6. Cot\n");
         scanf("%d",&select_option);
         RadianValue=0.017*Degrees_Value;
         switch(select_option)
         {
 
-            case 1: Sin(RadianValue);
+            case 1: Sin_function(RadianValue);
             break;
-            case 2: Cos(RadianValue);
+            case 2: Cos_function(RadianValue);
             break;
-            case 3: Tan(RadianValue);
+            case 3: Tan_function(RadianValue);
             break;
-            case 4: Sec(RadianValue);
+            case 4: Sec_function(RadianValue);
             break;
-            case 5: Cosec(RadianValue);
+            case 5: Cosec_function(RadianValue);
             break;
-            case 6: Cot(RadianValue);
+            case 6: Cot_function(RadianValue);
             break;
-
             default: printf("Inavlid Input!!!! Please choose Correct option\n");
             break;
         }
@@ -41,21 +46,21 @@ int main()
     }
     while(ask_User==1);
 }
-    void Sin(double RadianValue ){
+    void Sin_function(double RadianValue ){
         printf("\n %lf\n",sin(RadianValue));
     }
-    void Cos(double RadianValue ){
+    void Cos_function(double RadianValue ){
         printf("\n %lf\n",cos(RadianValue));
     }
-    void Tan(double RadianValue ){
+    void Tan_function(double RadianValue ){
         printf("\n %lf\n",tan(RadianValue));
     }
-    void Cosec(double RadianValue ){
+    void Cosec_function(double RadianValue ){
         printf("\n %lf\n",(1.0/sin(RadianValue)));
     }
-    void Sec(double RadianValue ){
+    void Sec_function(double RadianValue ){
         printf("\n %lf\n",(1.0/cos(RadianValue)));
     }
-    void Cot(double RadianValue ){
+    void Cot_function(double RadianValue ){
         printf("\n %lf\n",(1.0/tan(RadianValue)));
     }
