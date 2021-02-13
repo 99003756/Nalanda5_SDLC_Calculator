3628828412a374c2c7d30583864937bdbf297482
99003756 <78857426+99003756@users.noreply.github.com>
2021-02-13 16:15:29 +0530

    Add files via upload

---
+++ 3_Implementation/code.h
@@ -0,0 +1,38 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CODE_H__
+#define __CODE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//#include <stdio_ext.h>
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 + operand2
+*/
+int Area_calculation(int ip1,int ip2);
+
+/**
+*  subtracts the operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 - operand2
+*/
+int Volume_calculation(int ip1,int ip2);
+
+/**
+*  multiply the operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 * operand2
+*/
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
